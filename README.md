e3dc65241f9ad0a15f5a327c188d571d963f8536
GuIrene <irenegub94@gmail.com>
2022-08-22 13:49:06 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Package: bimodal_detector
+
+Run EM on genomic intervals to find bimodal regions
+
+
+## Usage
+
+basic install
+```
+pip install git+https://github.com/methylgrammarlab/bimodal_detector
+```
+alternatively, you may need to use an [access token](https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token)
+```
+pip install git+https://<PERSONAL ACCESS TOKEN>@github.com/methylgrammarlab/bimodal_detector.git
+```
+
+to run:
+```
+runEM -j <config.json>
+```
---
+++ setup.py
@@ -0,0 +1,32 @@
+import setuptools
+import os
+TOKEN_VALUE = os.getenv('EXPORTED_VAR_WITH_TOKEN')
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='bimodal_detector',
+    version='0.0.1',
+    author='Irene Unterman',
+    author_email='irene.guberman@mail.huji.ac.il',
+    description='fit two epistates to methylation sequencing',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/methylgrammarlab/bimodal_detector',
+    project_urls = {
+        "Bug Tracker": "https://github.com/methylgrammarlab/bimodal_detector/issues"
+    },
+    license='MIT',
+    packages=['bimodal_detector'],
+    install_requires=['numpy', 'pandas', 'scipy', 'bottleneck', "Click", "pytest"],
+                      # f"epiread-tools @ git+https://{TOKEN_VALUE}@github.com/methylgrammarlab/epiread-tools.git"
+                      # ],
+    include_package_data=True,
+    entry_points={
+    "console_scripts":[
+    "runEM = bimodal_detector.main:main",
+    ]
+    },
+)
+# python3 setup.py sdist bdist_wheel
